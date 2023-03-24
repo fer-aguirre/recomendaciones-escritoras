f6ebb377bfe6f398a92c3a72a95602a9c7b7f835
Fer Aguirre <kay_op@protonmail.com>
2023-03-24 16:13:04 -0600

    Add paths to load data

---
+++ recomendaciones_escritoras/data/load.py
@@ -0,0 +1,8 @@
+import recomendaciones_escritoras.utils.paths as paths
+
+
+# Directories and files
+data_raw = paths.data_raw_dir()
+data_processed = paths.data_processed_dir()
+escritoras_wiki = paths.data_raw_dir('escritoras_wiki.csv')
+goodreads_escritoras = paths.data_processed_dir('goodreads_escritoras.csv')
