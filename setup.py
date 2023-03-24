0ce6dfc093b0b18ea1a9543e80d2bd6be236046a
Fer Aguirre <kay_op@protonmail.com>
2023-03-24 13:12:19 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Recomendaciones Escritoras',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Sistema de recomendaciones de libros de escritoras latinoamericanas.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
